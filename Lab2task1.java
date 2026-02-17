0eb5e7486db8329f1d8caf3080e3f1b519a9f715
Amjad ali <amjadbhuttof@gmail.com>
2026-02-17 16:01:51 +0500

    Add files via upload

---
+++ Lab2task1.java
@@ -0,0 +1,15 @@
+public class Lab2task1 {
+    public static void main(String[] args) {
+    int num = 34;
+    double decimal = 3.14;
+    char alphabet = 'F';
+    boolean statement = true;
+
+    System.out.println("The value of int variable is: "+num);
+    System.out.println("The value of double variable is: "+decimal);
+    System.out.println("The value of char variable is: "+alphabet);
+    System.out.println("The value of statement variable is: "+statement);
+}
+}
+    
+
