70997e1fad7a57146651ffc69cf58c55e42e8d3a
Amjad ali <amjadbhuttof@gmail.com>
2026-02-17 15:56:02 +0500

    Add files via upload

---
+++ Msalary.java
@@ -0,0 +1,10 @@
+public class Msalary {
+    public static void main(String[] args) {
+      long mS = 50000;
+    for (int i = 1; i <= 6; i++){
+        System.out.println("monthly total  salary :"+ mS);
+        mS += 2000;
+    }
+    
+    }
+}
